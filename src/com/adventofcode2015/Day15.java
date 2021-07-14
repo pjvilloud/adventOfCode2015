4ef8f34f99bacfe1a83ab94d02969bc49dfaae7d
Pierre-Julien VILLOUD <pjvilloud@protonmail.com>
2021-07-14 16:08:54 +0200

    Completed day 15 part 1

---
+++ src/com/adventofcode2015/Day15.java
@@ -0,0 +1,106 @@
+package com.adventofcode2015;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Day15 {
+
+    static String input_test = """
+            Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+            Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
+            """;
+
+    static String input = """
+            Sugar: capacity 3, durability 0, flavor 0, texture -3, calories 2
+            Sprinkles: capacity -3, durability 3, flavor 0, texture 0, calories 9
+            Candy: capacity -1, durability 0, flavor 4, texture 0, calories 1
+            Chocolate: capacity 0, durability 0, flavor -2, texture 2, calories 8""";
+
+    final static int nbOfSpoons = 100;
+
+    public static void main(String[] args) {
+
+        class Ingredient {
+            public final String name;
+            public final short capacity;
+            public final short durability;
+            public final short flavor;
+            public final short texture;
+            public final short calories;
+
+            public Ingredient(String name, short capacity, short durability, short flavor, short texture, short calories) {
+                this.name = name;
+                this.capacity = capacity;
+                this.durability = durability;
+                this.flavor = flavor;
+                this.texture = texture;
+                this.calories = calories;
+            }
+
+            @Override
+            public String toString() {
+                final StringBuilder sb = new StringBuilder("Ingredient{");
+                sb.append("name='").append(name).append('\'');
+                sb.append(", capacity=").append(capacity);
+                sb.append(", durability=").append(durability);
+                sb.append(", flavor=").append(flavor);
+                sb.append(", texture=").append(texture);
+                sb.append(", calories=").append(calories);
+                sb.append('}');
+                return sb.toString();
+            }
+        }
+
+        List<Ingredient> ingredients = new ArrayList<>();
+        for(String line : input.split("\n")) {
+            String[] words = line.split(" ");
+            ingredients.add(new Ingredient(
+                    words[0].replace(":",""),
+                    Short.parseShort(words[2].replace(",","")),
+                    Short.parseShort(words[4].replace(",","")),
+                    Short.parseShort(words[6].replace(",","")),
+                    Short.parseShort(words[8].replace(",","")),
+                    Short.parseShort(words[10])
+            ));
+        }
+
+        List<List<Integer>> combinations = new ArrayList<>();
+        spoonCombinations(ingredients.size(), 100, new ArrayList<>(), combinations);
+
+        List<Integer> winningRecipe = null;
+        int lastSum = 0;
+
+        for (List<Integer> spoonCombination : combinations){
+            int sum = Math.max(0, ingredients.stream().mapToInt(ingredient -> ingredient.capacity * spoonCombination.get(ingredients.indexOf(ingredient))).sum()) *
+                    Math.max(0, ingredients.stream().mapToInt(ingredient -> ingredient.durability * spoonCombination.get(ingredients.indexOf(ingredient))).sum()) *
+                    Math.max(0, ingredients.stream().mapToInt(ingredient -> ingredient.flavor * spoonCombination.get(ingredients.indexOf(ingredient))).sum()) *
+                    Math.max(0, ingredients.stream().mapToInt(ingredient -> ingredient.texture * spoonCombination.get(ingredients.indexOf(ingredient))).sum());
+            if(sum > lastSum){
+                lastSum = sum;
+                winningRecipe = spoonCombination;
+            }
+        }
+
+        System.out.println("The highest-scoring cookie scores " + lastSum);
+        System.out.println("The winning-recipe is " + winningRecipe);
+
+    }
+
+    static void spoonCombinations(int size, int maxSum, ArrayList<Integer> previousList, List<List<Integer>> combinations){
+        ArrayList<Integer> tmpList = (ArrayList<Integer>) previousList.clone();
+
+        if(size == 1){
+            tmpList.add(maxSum);
+            combinations.add(tmpList);
+        }
+        else {
+            for (int i = 0; i < maxSum; i++) {
+                tmpList = (ArrayList<Integer>) previousList.clone();
+                tmpList.add(i);
+                spoonCombinations(size - 1, maxSum - i, tmpList, combinations);
+            }
+        }
+    }
+}
